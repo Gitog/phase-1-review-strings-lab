bce82a0a6c4566c2e289996bae5e70b3a3ed8412
James <jamesnjorio@gmail.com>
2022-08-19 16:41:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = "Gracie Hopper";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let slicedUser = currentUser.slice(0, 5);
+
+const shortGreeting = `Welcome, ${slicedUser}!`;
